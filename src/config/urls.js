29fdbfe936a121e8d95b939acb9de095f0f5c1c6
Cris <crispr.zde@gmail.com>
2024-09-09 12:37:28 +0200

    created urls.js

---
+++ src/config/urls.js
@@ -0,0 +1,16 @@
+export const BASE_URL = "http://localhost:3001";
+
+//public endpoints
+export const PUBLIC_URL = `${BASE_URL}/auth`;
+export const LOGIN_URL = `${PUBLIC_URL}/login`;
+export const SINGIN_URL = `${PUBLIC_URL}/singin`;
+
+//private endpoints 
+export const PRIVATE_URL = `${BASE_URL}/api`;
+export const GET_PUBLIC_IDEAS_URL = `${PRIVATE_UR}/public`;
+export const GET_BOARDS_URL = `${PRIVATE_UR}/board`;
+export const getBoardByIdUrl = (id) => `${PRIVATE_URL}/board/${id}`;
+export const getUserIdeas =(userId, categoryId) => `${PRIVATE_URL}/user/${userId}/category/${categoryId}`;
+export const NEW_IDEA_URL = `${PRIVATE_URL}/idea`;
+export const getUpdateIdeaUrl = (id) => `${PRIVATE_URL}idea/update/${id}`;
+export const getDeleteIdeaUrl = (id) => `${PRIVATE_URL}/idea/delete/${id}`;
